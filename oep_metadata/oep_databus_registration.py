59793ab6cbb6628b671b1d56334fa998c1283064
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2022-05-30 11:46:25 +0200

    Finished registration of OEP tables and submitting of metadata to MOSS

--- oep_metadata/oep_databus_registration.py
+++ oep_metadata/oep_databus_registration.py
@@ -10,7 +10,13 @@ from dev_databus_api_example import (
     deploy_to_databus,
     API_KEY,
     ACCOUNT_NAME,
+    DatabusError
 )
+from moss import submit_metadata_to_moss
+
+
+class MetadataError(Exception):
+    """Raised if metadata is invalid"""
 
 
 OEP_FOLDER = pathlib.Path(__file__).parent
@@ -61,32 +67,53 @@ def create_databus_group():
     deploy_to_databus(API_KEY, databus_group)
 
 
+def register_oep_table(schema_name, table_name):
+    metadata = get_table_meta(schema_name, table_name)
+
+    if len(metadata) == 0:
+        raise MetadataError(f"Metadata for table '{schema_name}.{table_name}' is empty.")
+    abstract = metadata.get("context", {}).get("documentation", "")
+    if not abstract:
+        raise MetadataError(f"Abstract for table '{schema_name}.{table_name}' is empty.")
+    try:
+        license_ = metadata["licenses"][0]["path"]
+    except (IndexError, KeyError) as e:
+        raise MetadataError(f"No license found for for table '{schema_name}.{table_name}'.") from e
+
+    files = [
+        DatabusFile(
+            f"{OEP_URL}/api/v0/schema/{schema_name}/tables/{table_name}/rows/",
+            cvs={},
+            file_ext="json",
+        )
+    ]
+
+    databus_version = DataVersion(
+        account_name=ACCOUNT_NAME,
+        group=GROUP,
+        artifact=table_name,
+        version=dt.date.today().isoformat(),
+        title=metadata["title"],
+        abstract=abstract,
+        description=metadata.get("description", ""),
+        license=license_,
+        databus_files=files,
+    )
+
+    deploy_to_databus(API_KEY, databus_version)
+
+    # Get file identifier:
+    databus_identifier = f"{databus_version.version_uri}/{databus_version.artifact}_{files[0].id_string}"
+    submit_metadata_to_moss(databus_identifier, metadata)
+
+
 def register_oep_tables():
     for schema_name in SCHEMAS:
         for table_name in get_tables(schema_name):
-            metadata = get_table_meta(schema_name, table_name)
-
-            files = [
-                DatabusFile(
-                    f"{OEP_URL}/api/v0/schema/{schema_name}/tables/{table_name}/rows/",
-                    cvs={},
-                    file_ext="json",
-                )
-            ]
-
-            databus_version = DataVersion(
-                account_name=ACCOUNT_NAME,
-                group=GROUP,
-                artifact=table_name,
-                version=dt.date.today().isoformat(),
-                title=metadata["title"],
-                abstract=metadata["context"]["documentation"],
-                description=metadata["description"],
-                license=metadata["license"]["name"],
-                databus_files=files,
-            )
-
-            deploy_to_databus(API_KEY, databus_version)
+            try:
+                register_oep_table(schema_name, table_name)
+            except (DatabusError, MetadataError) as e:
+                print(f"{e}\nSkipping registration for table '{schema_name}.{table_name}'")
 
 
 if __name__ == "__main__":
